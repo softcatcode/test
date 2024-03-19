e2d384ceb56b7fd553f423ccf240bd9935d1874a
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-03-19 21:28:17 +0300

    player setup and image asset were added

--- ServerInfoViewer/app/src/main/java/com/example/serverinfoviewer/presentation/ui/video/VideoFragment.kt
+++ ServerInfoViewer/app/src/main/java/com/example/serverinfoviewer/presentation/ui/video/VideoFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import com.example.serverinfoviewer.databinding.FragmentVideoBinding
 
 class VideoFragment : Fragment() {
@@ -12,6 +14,8 @@ class VideoFragment : Fragment() {
     private var _binding: FragmentVideoBinding? = null
     val binding: FragmentVideoBinding
             get() = _binding ?: throw RuntimeException("Video fragment binding is null")
+
+    private lateinit var exoPlayer: ExoPlayer
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,8 +26,36 @@ class VideoFragment : Fragment() {
         return binding.root
     }
 
+    private fun setupPlayer() {
+        exoPlayer = ExoPlayer.Builder(requireActivity()).build()
+        binding.playerView.player = exoPlayer
+        val item = MediaItem.fromUri(VIDEO_LINK)
+        exoPlayer.setMediaItem(item)
+        exoPlayer.prepare()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupPlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        exoPlayer.play()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        exoPlayer.pause()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        exoPlayer.stop()
+    }
+
+    companion object {
+        private const val VIDEO_LINK = "https://www.youtube.com/watch?v=QKWAvLeayec&ab_channel=MARGO"
     }
 }
