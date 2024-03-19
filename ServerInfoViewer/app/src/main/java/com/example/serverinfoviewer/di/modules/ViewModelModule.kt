e62ae16a996f54e9a4f60af742b5cc69b4ce48d5
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-03-20 02:04:59 +0300

    VideoViewModel is removed

--- ServerInfoViewer/app/src/main/java/com/example/serverinfoviewer/di/modules/ViewModelModule.kt
+++ ServerInfoViewer/app/src/main/java/com/example/serverinfoviewer/di/modules/ViewModelModule.kt
@@ -3,7 +3,6 @@ package com.example.serverinfoviewer.di.modules
 import androidx.lifecycle.ViewModel
 import com.example.serverinfoviewer.di.ViewModelKey
 import com.example.serverinfoviewer.presentation.ui.users.UsersViewModel
-import com.example.serverinfoviewer.presentation.ui.video.VideoViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -15,9 +14,4 @@ interface ViewModelModule {
     @ViewModelKey(UsersViewModel::class)
     @Binds
     fun bindUsersViewModel(impl: UsersViewModel): ViewModel
-
-    @IntoMap
-    @ViewModelKey(VideoViewModel::class)
-    @Binds
-    fun bindVideoViewModel(impl: VideoViewModel): ViewModel
 }
